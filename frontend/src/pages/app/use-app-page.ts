21acaef3dea888f6dacb57afcb22bef857acc6bc
Joaquin Catanzariti Torrens <joaquin.catanza@gmail.com>
2026-06-07 11:58:01 +0200

    fix NodeJS.Timeout

--- frontend/src/pages/app/use-app-page.ts
+++ frontend/src/pages/app/use-app-page.ts
@@ -20,7 +20,7 @@ export const useAppPage = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const chunksRef = useRef<Blob[]>([]);
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
   const recordedAudioRef = useRef<HTMLAudioElement>(null);
 
   const REFERENCE_TEXT = t('app.referenceText');
